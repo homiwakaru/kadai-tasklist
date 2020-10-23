be140e0e1cfdad151bef4de1f7e9d8048f772e31
miho.furukawa <homiwakarufu@gmail.com>
2020-10-23 12:45:29 +0000

    Fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
-       glash.now[:danger] = 'Task が投稿されませんでした'
+       flash.now[:danger] = 'Task が投稿されませんでした'
        render :new
     end 
       
@@ -47,10 +47,10 @@ class TasksController < ApplicationController
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
-
-private
-
-def task_params
-  params.require(:task).permit(:content)
-end  
